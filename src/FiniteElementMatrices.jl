e750eaa562d38bb18f84026cd7e1436afe319a32
Michael Hardman <29800382+mrhardman@users.noreply.github.com>
2026-01-26 10:52:57 +0000

    Refactor 2D matrix function for kernel function inputs.

--- src/FiniteElementMatrices.jl
+++ src/FiniteElementMatrices.jl
@@ -175,7 +175,7 @@ function finite_element_matrix(
                 ith_lpoly_data = lpoly_data.lpoly_data[i]
                 for l in 1:nquad
                     zzl = zz[l]
-                    funcz = kernel_function(scale*zzl+shift)
+                    funcz = kernel_function(scale*zzl+shift)::Float64
                     matrix[i,j,k] += (scale*wz[l]*funcz*
                             prefactor1*lagrange1(ith_lpoly_data,zzl)*
                             prefactor2*lagrange2(jth_lpoly_data,zzl)*
@@ -196,6 +196,25 @@ function finite_element_matrix(
     fn2_x2_type::LagrangeFunctionType,
     power_x2::Int64,
     coordinate_x2::ElementCoordinates
+    )
+    return finite_element_matrix(fn1_x1_type, fn2_x1_type, coordinate_x1,
+                fn1_x2_type, fn2_x2_type, coordinate_x2;
+                kernel_function=((v1,v2) -> (v1^power_x1)*(v2^power_x2)),
+                additional_quadrature_points_x1=power_x1,
+                additional_quadrature_points_x2=power_x2)
+end
+function finite_element_matrix(
+    fn1_x1_type::LagrangeFunctionType,
+    fn2_x1_type::LagrangeFunctionType,
+    coordinate_x1::ElementCoordinates,
+    fn1_x2_type::LagrangeFunctionType,
+    fn2_x2_type::LagrangeFunctionType,
+    coordinate_x2::ElementCoordinates;
+    # function of the "physical" coord v = s z + c
+    # rather than of the reference coordinate z on [-1,1]
+    kernel_function=((v1,v2) -> 1.0)::Function,
+    additional_quadrature_points_x1=0::Int64,
+    additional_quadrature_points_x2=0::Int64,
     )
     # coordinate x1 data
     lpoly_data_x1 = coordinate_x1.lpoly_data
@@ -220,13 +239,13 @@ function finite_element_matrix(
     prefactor21 = select_lagrange_prefactor(fn2_x1_type,scale_x1)
     prefactor22 = select_lagrange_prefactor(fn2_x2_type,scale_x2)
     # nquad chosen for exact results
-    nquad_x1 = ngrid_x1 + power_x1
+    nquad_x1 = ngrid_x1 + additional_quadrature_points_x1
     zz_x1, wz_x1 = gausslegendre(nquad_x1)
-    nquad_x2 = ngrid_x2 + power_x2
+    nquad_x2 = ngrid_x2 + additional_quadrature_points_x2
     zz_x2, wz_x2 = gausslegendre(nquad_x2)
     # compute integral
-    # int P_i(z) Q_j(z) poly(z) s d z
-    # with poly(z) = (s z + c)^power
+    # \int \int \left(P1_i(z_1) Q1_j(z_1) P2_i(z_1) Q2_j(z_2)
+    # kernel(s_1 z_1 + c_1, s_2 z_2 + c_2) \right) s_1 s_2 d z_1 d z_2
     # s = scale, c = shift and
     # P_i(z), Q_i(z) in [l_i(z), (1/s) d l_i(z) / d z]
     for jx2 in 1:ngrid_x2
@@ -239,17 +258,17 @@ function finite_element_matrix(
                     ix1_lpoly_data = lpoly_data_x1.lpoly_data[ix1]
                     for lx2 in 1:nquad_x2
                         zzl2 = zz_x2[lx2]
-                        polyz2 = get_polyz(power_x2,scale_x2,shift_x2,zzl2)
                         wgt2 = scale_x2*wz_x2[lx2]
                         lagrange_factor2 = (prefactor12*lagrange12(ix2_lpoly_data,zzl2)*
                                                prefactor22*lagrange22(jx2_lpoly_data,zzl2))
                         for lx1 in 1:nquad_x1
                             zzl1 = zz_x1[lx1]
-                            polyz1 = get_polyz(power_x1,scale_x1,shift_x1,zzl1)
                             wgt1 = scale_x1*wz_x1[lx1]
                             lagrange_factor1 = (prefactor11*lagrange11(ix1_lpoly_data,zzl1)*
                                                prefactor21*lagrange21(jx1_lpoly_data,zzl1))
-                            matrix[ix1,ix2,jx1,jx2] += (wgt1*polyz1*wgt2*polyz2*
+                            kernel_x1_x2 = kernel_function(scale_x1*zzl1+shift_x1,
+                                                        scale_x2*zzl2+shift_x2)::Float64
+                            matrix[ix1,ix2,jx1,jx2] += (wgt1*wgt2*kernel_x1_x2*
                                                         lagrange_factor1*
                                                         lagrange_factor2)
                         end
